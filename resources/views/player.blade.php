e2acaa9cb61f7085b9c584ffcf0eb716bea913eb
Elikill58 <arpetzouille@gmail.com>
2022-07-29 00:23:45 +0200

    Add name

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -3,21 +3,27 @@
 @section('title', 'Stats')
 
 <?php
-if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
-    config([
-        'database.connections.' . $settings->settings()->settings->database . '.driver' => 'mysql',
-        'database.connections.' . $settings->settings()->settings->database . '.host' => isSet($settings->settings()->settings->host) ? $settings->settings()->settings->host : env('DB_HOST', '127.0.0.1'),
-        'database.connections.' . $settings->settings()->settings->database . '.port' => isSet($settings->settings()->settings->port) ? $settings->settings()->settings->port : env('DB_PORT', '3306'),
-        'database.connections.' . $settings->settings()->settings->database . '.username' => isSet($settings->settings()->settings->username) ? $settings->settings()->settings->username : env('DB_USERNAME', 'root'),
-        'database.connections.' . $settings->settings()->settings->database . '.password' => isSet($settings->settings()->settings->password) ? $settings->settings()->settings->password : env('DB_PASSWORD', ''),
-        'database.connections.' . $settings->settings()->settings->database . '.database' => $settings->settings()->settings->database
-    ]);
-    $result = DB::connection($settings->settings()->settings->database)->select("SELECT * FROM " . $settings->settings()->settings->table . " WHERE " . $settings->settings()->settings->column_name . " = ?", [$uuid]);
-    $uuid = isset($result) && count($result) > 0 ? json_decode(json_encode($result[0]), true)[$settings->settings()->settings->column_uuid] : $uuid;
+$set = $settings->settings()->settings;
+config([
+    'database.connections.' . $set->database . '.driver' => 'mysql',
+    'database.connections.' . $set->database . '.host' => isSet($set->host) ? $set->host : env('DB_HOST', '127.0.0.1'),
+    'database.connections.' . $set->database . '.port' => isSet($set->port) ? $set->port : env('DB_PORT', '3306'),
+    'database.connections.' . $set->database . '.username' => isSet($set->username) ? $set->username : env('DB_USERNAME', 'root'),
+    'database.connections.' . $set->database . '.password' => isSet($set->password) ? $set->password : env('DB_PASSWORD', ''),
+    'database.connections.' . $set->database . '.database' => $set->database
+]);
+$isValidUUID = preg_match('/^\{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}\}?$/', strtoupper($uuid));
+$result = DB::connection($set->database)->select("SELECT * FROM " . $set->table . " WHERE " . ($isValidUUID ? $set->column_uuid : $set->column_name) . " = ?", [$uuid]);
+if(isset($result) && count($result) > 0) {
+    $dbResult = json_decode(json_encode($result[0]), true);
+    $uuid = $dbResult[$set->column_uuid];
+    $name = $dbResult[$set->column_name];
+} else {
+    $name = "Unknow: " . $uuid;
 }
 ?>
 
-@if($uuid == "test")
+@if($uuid == null)
     <script>window.location = "{{ route('stats.index') }}?error=not-found";</script>
     <?php exit; ?>
 @endif
@@ -28,48 +34,64 @@ if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89
 
 @section('content')
     <div class="row" id="stats">
-        @foreach($games as $game)
-            @if($game->stats()->count() >= 1)
-                <div class="col-md-3">
-                    <div class="card shadow">
-                        <div class="card-header rounded text-center text-primary">
-                            <i class="bi bi-controller fs-1 mb-3"></i>
+        <div class="col-9">
+            <div class="row">
+                @foreach($games as $game)
+                    @if($game->stats()->count() >= 1)
+                        <div class="col-md-3">
+                            <div class="card shadow">
+                                <div class="card-header rounded text-center text-primary">
+                                    <i class="bi bi-controller fs-1 mb-3"></i>
 
-                            <h2>{{ $game->name }}</h2>
-                            <p>{{ $game->description }}</p>
-                        </div>
-                        <div class="card-body rounded text-center text-primary">
-                            <?php
-                            $statsValues = $game->makeRequest($uuid);
-                            if(count($statsValues) == 0) {
-                                echo trans('stats::messages.error.never-played');
-                            } else {
-                                ?>
-                                @foreach ($statss as $stats)
-                                    @if($stats->games_id == $game->id)
-                                        <?php
-                                        $val = $stats->getValue($statsValues);
+                                    <h2>{{ $game->name }}</h2>
+                                    <p>{{ $game->description }}</p>
+                                </div>
+                                <div class="card-body rounded text-center text-primary">
+                                    <?php
+                                    $statsValues = $game->makeRequest($uuid);
+                                    if(count($statsValues) == 0) {
+                                        echo trans('stats::messages.error.never-played');
+                                    } else {
                                         ?>
-                                        @switch($stats->style ?? 1)
-                                            @case('1')
-                                            @include('stats::styles._basic')
-                                            @break
-                                            @case('2')
-                                            @include('stats::styles._ratio')
-                                            @break
-                                        @endswitch
-                                    @endif
-                                @endforeach
-                            <?php } ?>
+                                        @foreach ($statss as $stats)
+                                            @if($stats->games_id == $game->id)
+                                                <?php
+                                                $val = $stats->getValue($statsValues);
+                                                ?>
+                                                @switch($stats->style ?? 1)
+                                                    @case('1')
+                                                    @include('stats::styles._basic')
+                                                    @break
+                                                    @case('2')
+                                                    @include('stats::styles._ratio')
+                                                    @break
+                                                @endswitch
+                                            @endif
+                                        @endforeach
+                                    <?php } ?>
+                                </div>
+                            </div>
                         </div>
+                    @endif
+                @endforeach
+                @if($games->count() == 0)
+                    <div class="alert alert-warning" role="alert">
+                        {{ trans('stats::messages.stats-empty') }}
                     </div>
-                </div>
-            @endif
-        @endforeach
-        @if($games->count() == 0)
-            <div class="alert alert-warning" role="alert">
-                {{ trans('stats::messages.stats-empty') }}
+                @endif
             </div>
-        @endif
+        </div>
+        <div class="col-3">
+            <div class="card shadow">
+                <div class="card-header rounded text-center text-primary">
+                    <i class="bi bi-controller fs-1 mb-3"></i>
+
+                    <h2>{{ $name }}</p>
+                </div>
+                <div class="card-body rounded text-center text-primary">
+                    test
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
